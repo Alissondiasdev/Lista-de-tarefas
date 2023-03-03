2375fdae55a386d1b59c53f6f6d30be9c4ad0e45
alisson dias <alissondias120894@gmail.com>
2023-03-03 12:16:40 -0300

    implementando a funcionalidade de salvar os dados no localstorage

--- index.js
+++ index.js
@@ -3,59 +3,86 @@ var containerTarefas = document.getElementById('container__tarefas')
 const inputhidden = document.getElementById('inputhidden')
 const headerButton = document.getElementById('header-button')
 const headerbuttonatualizar = document.getElementById('header-button-atualizar')
-const novaTarefaArray = []
+
+const dataDisplay = document.getElementById("teste");
+let novaTarefaArray = []
 
 
 
 
 
-window.addEventListener("load", function() {
-    
+
+window.addEventListener("load", function () {
+    getlocalstorage()
+
     headerinput.focus();
+    
 });
 
-
-
-headerinput.addEventListener('keypress', function(evento){
-    if(evento.key == 'Enter' && headerinput.value > ''){
+const getlocalstorage = () => {
+    if(localStorage.getItem('tarefas')) {
+        novaTarefaArray = JSON.parse(localStorage.getItem('tarefas'));
+        adicionarTarefa();
         
+    }
+}
+
+
+
+headerinput.addEventListener('keypress', function (evento) {
+    if (evento.key == 'Enter' && headerinput.value > '') {
+
         novaTarefaObj = {
             nome: headerinput.value,
             id: (Math.floor(Math.random() * 10000))
         }
         novaTarefaArray.push(novaTarefaObj)
         adicionarTarefa()
-        headerinput.value= ("") 
-        
-        
-        containerTarefas.scrollTop = containerTarefas.scrollHeight;
-            
-        }
-    
-    })
+        headerinput.value = ("")
 
-    function criarTarefa () {
-        
-        if(headerinput.value > ''){
-            
-            novaTarefaObj = {
-                nome: headerinput.value,
-                id: (Math.floor(Math.random() * 10000))
-            }
-            novaTarefaArray.push(novaTarefaObj)
-            adicionarTarefa()
-            headerinput.value= ("") 
-           
-            
-        }
+
         containerTarefas.scrollTop = containerTarefas.scrollHeight;
+
     }
 
+})
+
+
+
+function criarTarefa() {
+    
+    if (headerinput.value > '') {
+        
+        novaTarefaObj = {
+            nome: headerinput.value,
+            id: (Math.floor(Math.random() * 10000))
+        }
+        novaTarefaArray.push(novaTarefaObj)
+       
+
+            
+
+        adicionarTarefa()
+
+        
+        headerinput.value = ("")
+
+
+    }
+    containerTarefas.scrollTop = containerTarefas.scrollHeight;
+    
+}
+
+  
 
 
 
 const adicionarTarefa = () => {
+    
+  
+
     const novaTarefa = novaTarefaArray.map((item) => {
+        
         return `
         
         <ul id="lista_tarefas" class="lista_tarefas">
@@ -73,20 +100,28 @@ const adicionarTarefa = () => {
         `
 
     })
+    
     containerTarefas.innerHTML = novaTarefa.join('')
-    containerTarefas.style.display="block"
+    containerTarefas.style.display = "block"
+    localStorage.setItem('tarefas', JSON.stringify(novaTarefaArray));
+
+
+
     
-    
-    
+
+
+
 }
 
 const editarTarefa = (id) => {
+
+
     if (id) {
-        headerButton.style.display="none"
+        headerButton.style.display = "none"
         const index = novaTarefaArray.findIndex((item) => {
             return item.id == id
         })
-        headerbuttonatualizar.style.display="flex"
+        headerbuttonatualizar.style.display = "flex"
         const task = novaTarefaArray[index]
         headerinput.value = task.nome
         inputhidden.value = task.id
@@ -94,12 +129,14 @@ const editarTarefa = (id) => {
 
 
     }
+    
 
 }
 
 const atualizarTarefa = () => {
-    headerButton.style.display="flex"
-    headerbuttonatualizar.style.display="block"
+
+    headerButton.style.display = "flex"
+    headerbuttonatualizar.style.display = "block"
     obj = {
 
         nome: headerinput.value,
@@ -117,30 +154,31 @@ const atualizarTarefa = () => {
 
 
 
-    
+
 
     adicionarTarefa()
-    headerinput.value= ("") 
+
+    headerinput.value = ("")
 
 }
 
 const excluirTarefa = (id) => {
     const indice = novaTarefaArray.findIndex((item) => {
-    return item.id == id;
+        return item.id == id;
     });
     if (indice === 0) {
-    // Executa a ação desejada
-    containerTarefas.style.display="none"
+        // Executa a ação desejada
+        containerTarefas.style.display = "none"
     }
     if (indice >= 0) {
-    novaTarefaArray.splice(indice, 1);
-    adicionarTarefa();
-    console.log(novaTarefaArray);
+        novaTarefaArray.splice(indice, 1);
+        adicionarTarefa();
+        console.log(novaTarefaArray);
     } else {
-    console.log("O array está vazio.");
+        console.log("O array está vazio.");
     }
-    };
+};
 
-    
-    
+
+
 
